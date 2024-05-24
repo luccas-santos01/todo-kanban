03feb2ddfc5072be27104dace5d03d31a5326190
Luccas Santos <luccasmsantos@outlook.com>
2024-05-24 08:50:31 -0300

    feat: criação do componente header

---
+++ src/components/Header/Header.styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: black;
+  color: white;
+  font-family: 'Roboto', sans-serif;
+
+  h2 {
+    margin: 0;
+    padding: 20px;
+  }
+`;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { HeaderContainer } from "./Header.styles";
+
+const Header: React.FC = () => {
+  return (
+    <HeaderContainer>
+      <h2>Lista de tarefas de bolso</h2>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
